c28c9ae2c8e493f72b32cb92bd171e38c50ac24a
codonssimplement <118065712+codonssimplement@users.noreply.github.com>
2024-06-30 20:08:49 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,22 @@ import HomePage from "./components/HomePage";
 import SignUpPage from "./components/SignUpPage";
 import DashboardPage from "./components/DashboardPage";
 
+import MyAppointmentsPage from "./components/MyappointmentsPage";
+import MyPatientsPage from "./components/MypatientsPage";
+
+import NewAppointmentPage from "./components/NewappointmentPage";
+import NewPatientPage from "./components/NewpatientPage";
+
+import AppointmentPage from "./components/AppointmentPage";
+import PatientPage from "./components/PatientPage";
+
+import MyAccount from "./components/MyAccount";
+
+
+
+
+
+
 function App() {
   return (
     <Router>
@@ -14,7 +30,23 @@ function App() {
         <Route path="/home" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/registration" element={<SignUpPage />} />
+
         <Route path="/dashboard" element={<DashboardPage />} />
+
+        <Route path="/dashboard/my-appointments" element={<MyAppointmentsPage />} />
+        <Route path="/dashboard/my-patients" element={<MyPatientsPage />} />
+
+        <Route path="/dashboard/new-appointment" element={<NewAppointmentPage />} />
+        <Route path="/dashboard/new-patient" element={<NewPatientPage />} />
+
+
+        <Route path="/dashboard/appointment" element={<AppointmentPage />} />
+        <Route path="/dashboard/patient" element={<PatientPage />} />
+
+
+        <Route path="/dashboard/my-account" element={<MyAccount />} />
+
+        
       </Routes>
     </Router>
   );
