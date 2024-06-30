adebdb3cd1aa85043c4d45c9f4df5eabbc324e56
codonssimplement <118065712+codonssimplement@users.noreply.github.com>
2024-06-30 16:03:28 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './components/LoginPage';
-import HomePage from './components/HomePage';
-import SignUpPage from './components/SignUpPage';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./components/LoginPage";
+import HomePage from "./components/HomePage";
+import SignUpPage from "./components/SignUpPage";
+import DashboardPage from "./components/DashboardPage";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/registration" element={ < SignUpPage/>} />
-        
+        <Route path="/registration" element={<SignUpPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
       </Routes>
     </Router>
   );
